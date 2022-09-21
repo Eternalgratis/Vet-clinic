aae385394febcb0e66b0906d301cca8d08370e90
Eternalgratis <tyaps002@gmail.com>
2022-09-21 21:46:14 +0100

    Double entry adjusted

--- data.sql
+++ data.sql
@@ -16,7 +16,6 @@ INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALU
 INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34),
 ('Jennifer Orwell', 19),
 ('Bob', 45),
-('Jennifer Orwell', 19),
 ('Melody Pond', 77),
 ('Dean Winchester', 14),
 ('Jodie Whittaker', 38);
