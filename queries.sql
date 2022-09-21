04cd79c469239f321cb2c24e527c88004f00809f
Eternalgratis <tyaps002@gmail.com>
2022-09-21 20:07:27 +0100

    Query multiple tables

--- queries.sql
+++ queries.sql
@@ -56,3 +56,27 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT MAX(escape_attempts)  FROM animals WHERE neutered = true OR false;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+/*queries join to answers questions */
+SELECT name FROM animals INNER JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Melody Pond';
+
+SELECT animals.name FROM animals INNER JOIN species ON animals.species_id= species.id WHERE species.name = 'Pokemon';
+
+SELECT owners.full_name, name FROM owners LEFT JOIN animals ON animals.owner_id = owners.id;
+SELECT species.name, COUNT(animals.name) FROM animals INNER JOIN species ON animals.species_id = species_id GROUP BY species.name;
+
+SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Jennifer Orwell'; 
+
+SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0; 
+
+SELECT owners.full_name, COUNT(animals.name) FROM owners LEFT JOIN animals ON owners.id = animals.owner_id GROUP BY owners.full_name ORDER BY COUNT(animals.name) DESC;
+
+
+-- Write queries (using JOIN) to answer the following questions:
+-- What animals belong to Melody Pond?
+-- List of all animals that are pokemon (their type is Pokemon).
+-- List all owners and their animals, remember to include those that don't own any animal.
+-- How many animals are there per species?
+-- List all Digimon owned by Jennifer Orwell.
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+-- Who owns the most animals?
--- schema.sql
+++ schema.sql
@@ -15,3 +15,41 @@ CREATE TABLE animals (
 /*Alter table*/
 ALTER TABLE animals
 ADD species VARCHAR(250);
+
+/* creating multiple tables */
+CREATE TABLE owners (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(250),
+    age INT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250),
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE animals
+DROP species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_animals_species FOREIGN KEY( species_id) REFERENCES species(id);
+
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_animals_owners FOREIGN KEY( owner_id) REFERENCES owners(id);
+
+
+
+
+
+
+
+-- Modify animals table:
+-- Make sure that id is set as autoincremented PRIMARY KEY
+-- Remove column species
+-- Add column species_id which is a foreign key referencing species table
+-- Add column owner_id which is a foreign key referencing the owners table
